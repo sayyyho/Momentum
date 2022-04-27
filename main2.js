193e9bee2a03f1a07739a8513bf1905bd5bd0dbc
sayyyho <323psh@naver.com>
2022-04-27 14:01:38 +0900

    study window event

--- main2.js
+++ main2.js
@@ -1,5 +1,4 @@
-const title = document.querySelector(".title:first-child h1");
-console.dir(title);
+const wordClick = document.querySelector(".title:first-child h1");
 
 function handleTitleClick(){
     console.log("now touch");
@@ -7,16 +6,35 @@ function handleTitleClick(){
 
 function handleMouseEnter(){
     console.log("blue");
-    title.innerHTML = "blue"
-    title.style.color = "blue";
+    wordClick.innerHTML = "blue"
+    wordClick.style.color = "blue";
 }
 
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowOffline(){
+    alert("no wifi");
+}
+
+function handleWindowOnline(){
+    alert("wifi에 접속됨")
+}
+
+function handleWindowCopy(){
+    alert("this page is copied");
+}
 function handleMouseLeave(){
     console.log("black");
-    title.innerHTML = "black";
-    title.style.color = "black";
-}
-title.addEventListener("click", handleTitleClick) // 함수 이름만 넘겨줘야 왼쪽 조건일때 함수 실행가능 ()표시는 x 
-//html element 가져와서 addEventListener 실행
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave);
+    wordClick.innerHTML = "black";
+    wordClick.style.color = "black";
+} 
+wordClick.addEventListener("mouseenter", handleMouseEnter);
+wordClick.addEventListener("mouseleave", handleMouseLeave); 
+wordClick.addEventListener("click", handleTitleClick); // 함수 이름만 넘겨줘야 왼쪽 조건일때 함수 실행가능 ()표시는 x 
+//html element 가져와서 addEventListener 실행 wordClick.addEventListener("mouseenter",handleMouseEnter); wordClick.addEventListener("mouseleave",handleMouseLeave);
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);

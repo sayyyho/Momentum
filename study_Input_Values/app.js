302cd28947a0a10cfb1d6b693c37fbf0e3b7a75e
sayyyho <323psh@naver.com>
2022-06-26 14:21:47 +0900

    memory username

--- study_Input_Values/app.js
+++ study_Input_Values/app.js
@@ -1,4 +1,4 @@
-const loginForm = document.querySelector(".login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 const user = document.querySelector("#greeting");
@@ -22,16 +22,29 @@ const user = document.querySelector("#greeting");
 // loginButton.addEventListener("click", buttonClick);
 
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
 function onSubmit(event){ //event 변수는 관행적으로 이렇게 명명하며,
     // 만약에 존재하면 방금 일어났던 정보를 받겠다는 것이고 알아서 정보가 들어옴. 
     event.preventDefault(); // 어떤 event의 기본 행동을 막는 부분. 기본 제공 함수.
    // 원래 실행 되어야 하는 브라우저의 일을 막음(새로운 링크로 이동이 안됨)
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASS);
+    paintGreeting(username);
+}
+
+function paintGreeting(username){
     user.innerHTML = `Hello ${username}`;
     user.classList.remove(HIDDEN_CLASS);
 }
 
 
-loginForm.addEventListener("submit", onSubmit); // 2번째 인자인 함수명을 주면 js가 알아서 함수 실행을 해줌 // 그 증거로 함수내에서 변수를 하나 추가하면 정보에 관한 오브젝트를 받을 수 있음
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit", onSubmit); // 2번째 인자인 함수명을 주면 js가 알아서 함수 실행을 해줌 // 그 증거로 함수내에서 변수를 하나 추가하면 정보에 관한 오브젝트를 받을 수 있음
+} else {
+    paintGreeting(savedUsername);
+}
